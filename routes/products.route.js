e8066d1aa32bf5f4baecbb824527dfd9cfa631ce
Patricio Sirena <patobsirena@gmail.com>
2024-10-07 12:23:27 -0300

    Feat: Rutas de usuarios y productos
    Nombre: Patricio Sirena.
    Fecha: 07/10/24.
    Tipo de commit: Feat - Agrego las rutas de usuarios y productos.

--- routes/products.route.js
+++ routes/products.route.js
@@ -1,6 +1,34 @@
 const express = require('express')
-const route = express.Router()
+const router = express.Router()
 
-// route.post('/', createProduct)
+router.post('/')
 
-module.exports = route
+router.post('/mainPicture/:productId')
+
+router.post('/addProductImage/:productId')
+
+router.post('/productState/:productId')
+
+router.post('/addToCart/:productId')
+
+router.post('/delFromCart/:productId')
+
+router.post('/addToFavorite/:productId')
+
+router.post('/delFromFavorite/:productId')
+
+router.get('/')
+
+router.get('/:productId')
+
+router.get('/getCart')
+
+router.get('/getFavorites')
+
+router.put('/:productId')
+
+router.delete('/delProductImage/:productId/galery/:imageId')
+
+router.delete('/:productId')
+
+module.exports = router
