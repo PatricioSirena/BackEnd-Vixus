628e8c1d9551966bc5505b9367b5a1743fb457cc
Patricio Sirena <patobsirena@gmail.com>
2024-10-08 09:35:26 -0300

    Feat: Log4JS
    Nombre: Patricio Sirena.
    Fecha: 08/10/2024.
    Tipo de commit: Feat - Instalo y configuro libreria Log4JS para el registro de los logs.

---
+++ helpers/logger.js
@@ -0,0 +1,15 @@
+const log4js = require('log4js')
+
+log4js.configure({
+    appenders: {
+        console: { type: 'console' },
+        file: { type: 'file', filename: 'app.log' }
+    },
+    categories: {
+        default: { appenders: ['console', 'file'], level: 'debug' }
+    }
+})
+
+const logger = log4js.getLogger()
+
+module.exports = logger;
